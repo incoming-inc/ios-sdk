4900ca7ec8797f0a192b78f0870b7a184a12cc82
Sebastien Ardon <sebastien@incoming-media.com>
2018-03-05 10:16:11 +1100

    Documentation updates

--- Swift/PVNSampleSwift/AppDelegate.swift
+++ Swift/PVNSampleSwift/AppDelegate.swift
@@ -7,7 +7,6 @@
 //
 
 import UIKit
-import UserNotifications
 import IncomingPVN
 
 @UIApplicationMain
@@ -25,16 +24,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate, UNUserNotificationCenterD
         // You must call this method at some stage for the push video service to operate correctly. 
         ISDKAppDelegateHelper.registerForRemoteNotifications()
         
-        // This will pop-up the OS permission dialog, feel free to
-        // integrate them differently in your workflow, e.g. after prompting the user if they'd agree to do so
-        ISDKAppDelegateHelper.registerForNotifications()
-        
-        // set UNNUserNotificationCenter delegate
-        if #available(iOS 10.0, *) {
-            let notificationCenter = UNUserNotificationCenter.current()
-            notificationCenter.delegate = self
-        }
-        
         // the two following calls are optional. They enable location and motion data collection
         // which improves the timing prediction of Push Video Notifications.
         // Calling these methods may result in the OS permission dialog being presented
@@ -45,31 +34,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate, UNUserNotificationCenterD
         // <insert your app initialization code here>
         return true
     }
-    
-    
-    func application(_ application: UIApplication, open url: URL, sourceApplication: String?, annotation: Any) -> Bool {
-        
-        // ISDK method forward
-        if (ISDKAppDelegateHelper.handleOpen(url, sourceApplication: sourceApplication!, annotation: annotation) == false)
-        {
-            // perform handling of your app URL here
-            
-        }
-        return true
-    }
-    
+
     
     func application(_ application: UIApplication, performFetchWithCompletionHandler completionHandler: @escaping (UIBackgroundFetchResult) -> Void) {
         
         // ISDK method forward
         ISDKAppDelegateHelper.application(application, performFetchWithCompletionHandler:completionHandler)
-        
-    }
-    
-    func application(_ application: UIApplication, handleEventsForBackgroundURLSession identifier: String, completionHandler: @escaping () -> Void) {
-        
-        // ISDK method forward
-        ISDKAppDelegateHelper.application(application, handleEventsForBackgroundURLSession: identifier, completionHandler: completionHandler)
         
     }
     
@@ -95,28 +65,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate, UNUserNotificationCenterD
 
         }
     }
-    
-    func application(_ application: UIApplication, didReceive notification: UILocalNotification) {
-        
-        // ISDK method forward
-        if (ISDKAppDelegateHelper.application(application, didReceive: notification) == false) {
-            // process your app local notification here
-            
-        }
-    }
-    
-    
-    func application(_ application: UIApplication, handleActionWithIdentifier identifier: String?, for notification: UILocalNotification, completionHandler: @escaping () -> Void) {
-        
-        // ISDK method forward
-        if (ISDKAppDelegateHelper.application(application, handleActionWithIdentifier: identifier, for: notification, completionHandler: completionHandler) == false) {
-            
-            // process your app local notification here
-            // when done, call the OS completion block
-            completionHandler();
-        }
-    }
-    
+
 
     func applicationWillResignActive(_ application: UIApplication) {
     }
@@ -132,33 +81,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate, UNUserNotificationCenterD
 
     func applicationWillTerminate(_ application: UIApplication) {
     }
-    
-    @available(iOS 10.0, *)
-    func userNotificationCenter(_ center: UNUserNotificationCenter, didReceive response: UNNotificationResponse, withCompletionHandler completionHandler: @escaping () -> Void) {
-        
-        // forward to ISDK
-        ISDKAppDelegateHelper.userNotificationCenter(center, didReceive: response) { (processed: Bool) -> Void in
-            
-            if !processed {
-                // this notification is not ISDK, handle your app notification response here as needed
-            }
-            
-            // when done, call the OS completion block
-            completionHandler()
-        }
-    }
-    
-    @available(iOS 10.0, *)
-    func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
-        
-        ISDKAppDelegateHelper.userNotificationCenter(center, willPresent: notification) { (processed: Bool) -> Void in
-            if !processed {
-                // this notification is not ISDK
-                // process your app notification here
-                // and call the completion handler if needed ..
 
-            }
-        }
-    }
 }
 
